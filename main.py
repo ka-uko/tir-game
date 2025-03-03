996c24c94f26550dd7bdb0bb8868b3004c3279ef
ka-uko <ka.uko@yandex.ru>
2025-03-03 16:25:42 +0300

    app cycle cod

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ icon = pygame.image.load("img/img.png")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/img_1.png")
-target_width = 80
-target_height = 80
+target_width = 60
+target_height = 60
+target_img = pygame.transform.scale(target_img, (target_width, target_height))
+
 target_x = random.randint (0, SCREEN_WIDTH - target_width)
 target_y = random.randint (0, SCREEN_HEIGHT - target_height)
 
@@ -23,7 +25,20 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+
+
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 
 
